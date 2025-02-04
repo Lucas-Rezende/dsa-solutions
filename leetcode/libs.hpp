a44946183018bfdbcff30c8dcf95d5ab91e75a8d
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2025-02-03 22:36:24 -0300

    add "contains duplicate" problem

---
+++ leetcode/libs.hpp
@@ -0,0 +1,32 @@
+#ifndef LIBS
+#define LIBS
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+#include <string>
+#include <cstring>
+#include <bitset>
+#include <tuple>
+#include <utility>
+#include <memory>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+#endif
