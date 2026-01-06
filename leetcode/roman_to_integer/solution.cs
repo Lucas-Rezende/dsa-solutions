0745a883d0ff2547ed06b990d2f57edefcf3d43e
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2026-01-06 16:37:14 -0300

    update RomanToInteger (C#)

--- leetcode/roman_to_integer/solution.cs
+++ leetcode/roman_to_integer/solution.cs
@@ -1,12 +1,27 @@
-public class RomanToInteger (int value)
-{
-    public static void Main()
-    {
-        int id = 456; // Hardcoded value
-        Console.WriteLine($"Using hardcoded ID: {id}");
+using System;
+using System.Collections.Generic;
 
-        OuterClass outerInstance = new OuterClass("ExampleName", id);
-        outerInstance.DisplayInfo();
+public class Solution
+{
+    public int RomanToInteger(string s)
+    {
+        Dictionary<char, int> romanNumbers = new Dictionary<char, int> {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        }
+        int arabicNumeral = 0;
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            int currentValue = romanNumbers[s[i]];
+            int nextValue = romanNumbers[s[i+1]];
+
+            if (currentValue >= nextValue) { arabicNumeral += currentValue}
+            else
+            {
+                arabicNumeral += (currentValue - nextValue); i++;
+            }
+
+        }
+        return arabicNumeral;
     }
 }
-
